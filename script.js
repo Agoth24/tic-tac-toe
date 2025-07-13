37a8af1e4ad4f5e0fef822b97c1b48f568a6cab2
Agoth24 <agoth.a3@gmail.com>
2025-07-13 12:35:01 -0600

    Finished the game and cleaned up lots of logic
    - Removed redundancies and fixed some bugs with the end
    of game conditions.
    
    - Added some comments to explain code snippets better.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const gameBoard = (() => {
   const board = Array(9).fill("");
   function getBoard() {
+    // Returns the board array
     return board;
   }
   function setCell(cell, marker) {
@@ -10,10 +11,15 @@ const gameBoard = (() => {
       board[cell] = marker;
     }
   }
-  function clearBoard(board) {
-    board = Array(9).fill("");
+  function clearBoard() {
+    // Loop through all elements in the board
+    for (let i = 0; i < board.length; i++) {
+      // Clear each element
+      board[i] = "";
+    }
   }
 
+  // PUBLIC API
   return { getBoard, setCell, clearBoard };
 })();
 
@@ -21,6 +27,7 @@ const gameBoard = (() => {
 function renderBoard() {
   // Board is an array
   const board = gameBoard.getBoard();
+
   // Iterate over the board and fill each DOM div
   // with the corresponding cell
   board.forEach((cell, index) => {
@@ -34,61 +41,155 @@ const Player = (name, marker) => {
   function makeMove(cell) {
     gameBoard.setCell(cell, marker);
   }
+
+  // Returns Player Object
   return { name, marker, makeMove };
 };
 
+// GAME CONTROLLER MODULE
 const Game = (() => {
+  // STATE
   let players;
   let gameOver;
   let winner;
   let currentPlayerIndex;
-  
-  
-    function init() {
-    players = [Player("Player 1", "X"), Player("Player 2", "O")];
-    gameOver = false;
-    winner = null;
-    currentPlayerIndex = 0;
-  }
 
+  // BEHAVIOUR
+
+  // Start a new game
+  function init() {
+    players = [Player("Player 1", "X"), Player("Player 2", "O")];
+    currentPlayerIndex = 0;
+    resetState();
+
+    // Listen to each cell and run a function which takes the cell and index
+    document.querySelectorAll(".cell").forEach((cell, index) => {
+      cell.addEventListener("click", () => {
+        const board = gameBoard.getBoard();
+        if (!board[index] && !gameOver) {
+          playRound(index);
+        }
+      });
+    });
+
+    document.querySelector(".restart").addEventListener("click", restartGame);
+  }
+  // Grab the object of the current player
   function getCurrentPlayer() {
     return players[currentPlayerIndex];
   }
+
+  // Runs for each move (Put in a loop later)
   function playRound(cell) {
     const player = getCurrentPlayer();
     player.makeMove(cell);
-    const result = checkGameOver()
     renderBoard();
-    switchPlayer()
+
+    const board = gameBoard.getBoard();
+    // Get the current result of the game
+    if (checkGameOver(board)) {
+      gameOver = true;
+      winner = getCurrentPlayer();
+      GameMessage.displayWinner(winner);
+      return;
+    }
+    if (isDraw()) {
+      gameOver = true;
+      GameMessage.displayDraw();
+      return;
+    }
+
+    // If the game isn't over...
+    switchPlayer();
   }
 
-  function switchPlayer() {}
-  currentPlayerIndex = 1 - currentPlayerIndex;
+  // Switches from index 0 to 1 and vice versa
+  function switchPlayer() {
+    currentPlayerIndex = 1 - currentPlayerIndex;
+  }
 
   // Checks if the game is over
   function checkGameOver(board) {
-    const winConditions = [];
+    const winConditions = [
+      [0, 1, 2], // Rows
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6], // Cols
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8], // Diagonals
+      [2, 4, 6],
+    ];
+    // For three cells in a row...
+    for (const [a, b, c] of winConditions) {
+      // If they are all the same...
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        // Game is over
+        return true;
+      }
+    }
+    // Game is not over
+    return false;
   }
 
-  function displayWinner(winner) {}
+  // Return true if every cell in the board is occupied
+  function isDraw() {
+    return gameBoard.getBoard().every((cell) => cell !== "");
+  }
 
-  function restartGame() {
+  // Reset state variables and clear the board
+  function resetState() {
+    gameOver = false;
+    winner = null;
     gameBoard.clearBoard();
     renderBoard();
   }
+
+  // To be run after at least one game is completed
+  function restartGame() {
+    resetState();
+    GameMessage.clearWinMessage();
+  }
+
+  // PUBLIC API FOR GAME MODULE
   return {
     init,
     getCurrentPlayer,
     playRound,
-    switchPlayer,
-    checkGameOver,
-    displayWinner,
     restartGame,
   };
 })();
 
-function checkWinner(board) {}
+// IIFE for a global module that prints messages
+const GameMessage = (() => {
+  // Grab the message box div
+  const messageBox = document.querySelector(".message-box");
 
-function isDraw(board) {}
+  // Function for adding text to the div
+  function displayText(message) {
+    messageBox.textContent = message;
+  }
+
+  // Formatting win message
+  function winMessage(winner) {
+    return `${winner.name} wins!`;
+  }
+
+  // Diplaying the winner
+  function displayWinner(player) {
+    displayText(winMessage(player));
+  }
+
+  // Displaying a Draw
+  function displayDraw() {
+    displayText(`It's a draw!`);
+  }
+
+  // Clears win message
+  function clearWinMessage() {
+    displayText("");
+  }
+  return { displayWinner, displayDraw, clearWinMessage };
+})();
 
 Game.init();
